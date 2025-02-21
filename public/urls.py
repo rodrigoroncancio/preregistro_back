c26f55dcfacf82d7e40ca9e978c482b2b9d9315c
Jorge Arenas <jaaparenas@gmail.com>
2025-02-20 23:54:53 -0700

    Validacion documento

--- public/urls.py
+++ public/urls.py
@@ -1,11 +1,12 @@
 from rest_framework import routers
 from django.urls import path
 
-from .views.catatumbo import CatatumboPreregistroView
+from .views.catatumbo import *
 
 router = routers.SimpleRouter()
 #router.register(r'customer', CustomerViewSet, basename='customer')
 
 urlpatterns = [
+    path('forms/catatumbo/validar_documento/', CatatumboValidaDocumentoView.as_view(), name='catatumbo-valida-documento'),
     path('forms/catatumbo/preregistro/', CatatumboPreregistroView.as_view(), name='catatumbo-preregistro'),
 ]
--- public/views/catatumbo.py
+++ public/views/catatumbo.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from ..models import FormCatatumboPreregistro
 
 from ..serializers.catatumbo_preregistro import CatatumboPreregistroSerializer
 
@@ -12,3 +13,13 @@ class CatatumboPreregistroView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CatatumboValidaDocumentoView(APIView):
+
+    def get(self, request):
+        documento = request.query_params.get('documento')
+
+        # Completar esta logica
+        existeDocumento = FormCatatumboPreregistro.objects.filter(numero_documento=documento).exists()
+        return Response(existeDocumento, status=status.HTTP_200_OK)
