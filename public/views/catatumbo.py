aaa2db6d5abc2c8bd61ccb8b1513c83f59fbe89f
rodrigo roncancio <rodrigo.roncancio@expansionti.com>
2025-03-01 16:53:58 -0500

    se retrocede en validacion de usuarios argelia

--- public/views/catatumbo.py
+++ public/views/catatumbo.py
@@ -77,46 +77,46 @@ class ArgeliaFichaValidaDocumentoView(APIView):
             status=status.HTTP_200_OK
         ) 
 
-        if ValidationArgeliaPersonas.objects.filter(numero_documento=documento).exists():
-            # Buscar si el documento existe
-            registro = ArgeliaPersonas.objects.filter(identificacion=documento).first()
+        # if ValidationArgeliaPersonas.objects.filter(numero_documento=documento).exists():
+        # Buscar si el documento existe
+        registro = ArgeliaPersonas.objects.filter(identificacion=documento).first()
 
-            if registro:
-                # Serializar el registro encontrado
-                serializer = ArgeliaPersonasSerializer(registro)
-                return Response(
-                    {
-                        "status": 1,
-                        "data": serializer.data
-                    },
-                    status=status.HTTP_200_OK
-                )
-            # Si no existe, devolver estructura estándar con `success: false` y `data: {}`
+        if registro:
+            # Serializar el registro encontrado
+            serializer = ArgeliaPersonasSerializer(registro)
             return Response(
                 {
-                    "status": 3,
-                    "data": {}
+                    "status": 1,
+                    "data": serializer.data
                 },
                 status=status.HTTP_200_OK
-            )   
-
-        # Si no existe, devolver estructura estándar con `success: false` y `data: {}`
-        registro = ArgeliaPersonas.objects.filter(identificacion=documento).first()
-        if registro:
-            return Response(
-            {
-                "status": 4,
-                "data": {}
-            },
-            status=status.HTTP_200_OK
-        )  
-        else :
-            return Response(
+            )
+            # Si no existe, devolver estructura estándar con `success: false` y `data: {}`
+        return Response(
             {
                 "status": 3,
                 "data": {}
             },
-            status=status.HTTP_200_OK)
+            status=status.HTTP_200_OK
+        )   
+
+        # Si no existe, devolver estructura estándar con `success: false` y `data: {}`
+        # registro = ArgeliaPersonas.objects.filter(identificacion=documento).first()
+        # if registro:
+        #     return Response(
+        #     {
+        #         "status": 4,
+        #         "data": {}
+        #     },
+        #     status=status.HTTP_200_OK
+        # )  
+        # else :
+        #     return Response(
+        #     {
+        #         "status": 3,
+        #         "data": {}
+        #     },
+        #     status=status.HTTP_200_OK)
     
     
 class ArgeliaFichaAcuerdoNucleoView(APIView):
